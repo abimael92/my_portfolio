7701e513ecbc4e2c1289bcfdcea06d57a28edf71
Abimael <abimael1992g@gmail.com>
2025-05-21 00:38:55 -0600

    add techs

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiFirebase, DiReact, DiZend, DiGithubBadge, DiScrum } from 'react-icons/di';
+import { SiDocker, SiFigma } from 'react-icons/si';
+import { FaCodeBranch } from 'react-icons/fa';
+
+
+
 import {
 	Section,
 	SectionDivider,
 	SectionText,
 	SectionTitle,
+	SectionSubtitle
 } from '../../styles/GlobalComponents';
 import {
 	List,
+	List2,
 	ListContainer,
 	ListItem,
 	ListParagraph,
@@ -40,52 +47,66 @@ const Technologies = () => (
 	<Section id='tech'>
 		<SectionTitle>Technologies </SectionTitle>
 		<SectionDivider />
-
 		<SectionText>
-			I have hands-on experience across the full web development stack,
-			including front-end, back-end, and design technologies.
-			My skill set enables me to build seamless, efficient, and visually compelling applications
-			that deliver outstanding user experiences.
+			I specialize in full-stack development, creating performant and user-centric web applications.
+			My expertise spans modern front-end frameworks, scalable back-end architectures, and user-focused design,
+			enabling me to deliver seamless digital experiences that solve real-world problems.
 		</SectionText>
-
 		<List>
 			<ListItem>
-				<picture>
-					<DiReact size='3rem' />
-				</picture>
+				<picture><DiReact size="3rem" /></picture>
 				<ListContainer>
-					<ListTitle>Front-End</ListTitle>
+					<ListTitle>Frontend</ListTitle>
 					<ListParagraph>
-						Experiece with <br />
-						React.js
+						React + TypeScript for performant UIs. Built responsive, accessible apps with real-world user flows.
 					</ListParagraph>
 				</ListContainer>
 			</ListItem>
+
 			<ListItem>
-				<picture>
-					<DiFirebase size='3rem' />
-				</picture>
+				<picture><DiFirebase size="3rem" /></picture>
 				<ListContainer>
-					<ListTitle>Back-End</ListTitle>
+					<ListTitle>Backend</ListTitle>
 					<ListParagraph>
-						Experience with <br />
-						Node and Databases
+						Node.js APIs with MongoDB/SQL. Scalable services, auth flows, and secure data handling.
 					</ListParagraph>
 				</ListContainer>
 			</ListItem>
+
 			<ListItem>
-				<picture>
-					<DiZend size='3rem' />
-				</picture>
+				<picture><SiDocker size="3rem" /></picture>
 				<ListContainer>
-					<ListTitle>UI/UX</ListTitle>
+					<ListTitle>DevOps</ListTitle>
 					<ListParagraph>
-						Experience with <br />
-						tools like Figma
+						CI/CD pipelines, Docker containers, AWS S3 deploys. Delivered code to prod reliably.
 					</ListParagraph>
 				</ListContainer>
 			</ListItem>
 		</List>
+
+		<List2>
+			<ListItem>
+				<picture><SiFigma size="3rem" /></picture>
+				<ListContainer>
+					<ListTitle>UI / UX</ListTitle>
+					<ListParagraph>
+						Figma to code. Created modern layouts and smooth flows focused on usability and clarity.
+					</ListParagraph>
+				</ListContainer>
+			</ListItem>
+
+			<ListItem>
+				<picture><FaCodeBranch size="3rem" /></picture>
+				<ListContainer>
+					<ListTitle>Teamwork</ListTitle>
+					<ListParagraph>
+						Agile sprints, GitHub PRs, cross-team collab. Delivered features fast with strong team synergy.
+					</ListParagraph>
+				</ListContainer>
+			</ListItem>
+		</List2>
+
+		<SectionSubtitle>Skills </SectionSubtitle>
 		<SectionDivider colorAlt />
 
 		<SkillsContainer>
