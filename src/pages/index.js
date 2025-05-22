05aab6297492336fa8676d6ddcd9538478ecefaa
Abimael <abimael1992g@gmail.com>
2025-05-22 17:42:36 -0600

    fix tech dynamic view

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
+import dynamic from 'next/dynamic';
 import Acomplishments from '../components/Achievements/Achievements';
 import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
 import Hero from '../components/Hero/Hero';
 import Projects from '../components/Projects/Projects';
-import Technologies from '../components/Technologies/Technologies';
+// import Technologies from '../components/Technologies/Technologies';
 import Experience from '../components/Experience/Experience';
 import About from '../components/About/About';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
+
+const Technologies = dynamic(() => import('../components/Technologies/Technologies'), { ssr: false });
 
 const Home = () => {
 	return (
