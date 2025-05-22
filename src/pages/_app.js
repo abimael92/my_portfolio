4adf77bd64ffe84b9d312af332ae4b3bb48a4a61
Abimael <abimael1992g@gmail.com>
2025-05-22 17:44:19 -0600

    verify if access is granted

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,17 @@
+import { useState, useEffect } from 'react';
 import Theme from '../styles/theme';
 
 export default function App({ Component, pageProps }) {
+  const [accessGranted, setAccessGranted] = useState(false);
+
+  useEffect(() => {
+    const allowed = localStorage.getItem('access_granted');
+    setAccessGranted(Boolean(allowed));
+  }, []);
+
   return (
-    <>
-      <Theme>
-        <Component {...pageProps} />
-      </Theme>
-    </>
+    <Theme>
+      <Component {...pageProps} accessGranted={accessGranted} />
+    </Theme>
   );
 }
- 
